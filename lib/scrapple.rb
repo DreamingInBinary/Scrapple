a1ff0990e284b420be6a61ca3154ffd2268d9e16
Jordan Morgan <the_morgan_fam@yahoo.com>
2021-03-04 08:59:01 -0600

    Write to json

--- lib/scrapple.rb
+++ lib/scrapple.rb
@@ -7,6 +7,7 @@ require 'parallel'
 require 'kimurai'
 require 'nokogiri'
 require 'odyssey'
+require 'json'
 
 class Scrapple < Kimurai::Base
   @engine = :selenium_firefox
@@ -20,16 +21,23 @@ class Scrapple < Kimurai::Base
     # Due to the docs being generated using next.js - wait a few seconds for them to be ready to crawl.
     sleep 2
   
+    File.open("tmp.json","w") do |f|
+      f.write("{\"frameworks\": {")
+    end
+    
     Parallel.each(response.css('ul.list li')) do |framework|
       framework_data = {}
       framework_data[:name] = framework.css("div.card__content").css("p").text
       framework_data[:description] = framework.css("div.card__content").css("div.card__abstract").text
       framework_data[:href] = 'https://developer.apple.com' + framework.css("a.card")[0]['href']
       
-      puts "#{framework_data.inspect}"
+      # puts "#{framework_data.inspect}"
       
-      # Next up - crawl each framework -> each symbol -> write to .json
-
+      # Next up - See how to know when request is done, and append valid JSON braackets. Crawl each framework -> each symbol -> write to .json
+      File.open("tmp.json","a") do |f|
+        f.write("\"#{framework_data[:name]}\":" + "#{framework_data.to_json}" + ',')
+      end
+      
     rescue StandardError => e
       puts "Failed a request for the #{framework_data[:name].to_s} framework: (#{e.inspect}), moving on."
 
