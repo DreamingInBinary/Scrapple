cbcd67081b8be2a5251f16bf4feb30f444f30e5c
Jordan Morgan <the_morgan_fam@yahoo.com>
2021-03-09 13:44:11 -0600

    Closer still

--- lib/scrapple.rb
+++ lib/scrapple.rb
@@ -13,7 +13,8 @@ require 'json'
 # This current code gets all of the framework json, and from there it's just
 # A matter of parsing that and getting the docs recursively for each.
 
-# Next up: Get docs appending to array correctly
+# Next up: It doesn't seem to deep-search collection groups. For example, this was not found in Accelerate: https://developer.apple.com/documentation/accelerate/vdsp
+# So compare that json with what I had and see why it doesn't make a request to it.
 
 class Scrapple < Kimurai::Base
   @engine = :selenium_firefox
@@ -21,12 +22,12 @@ class Scrapple < Kimurai::Base
   @config = {
     disable_images: true,
     skip_duplicate_requests: true,
-    retry_request_errors: [Net::HTTPNotFound]
+    retry_request_errors: [Net::HTTPNotFound,Net::ReadTimeout,Net::HTTPBadGateway]
   }
   
   def parse(response, url:, data: {})
     
-    scrape_accelerate_only = 1
+    scrape_test_only = 0
     framework_data = JSON.parse(response.css("div#json")[0])["references"]
     sorted_frameworks = Array.new
 
@@ -43,52 +44,69 @@ class Scrapple < Kimurai::Base
       end 
     end
 
-    # Sort them by name and save off to json to append later
+    # Sort them by name
     frameworks_hash = {}
     sorted_frameworks.sort_by! { |topic| topic["name"].downcase }
-    sorted_frameworks.each do |val|
-      frameworks_hash[val["name"]] = val
+    sorted_frameworks.each do |f|
+      file_name = sanitize_filename f["name"]
+      f_hash = {"docs":{}}
+      File.write("#{file_name}.json", JSON.dump(f_hash))
     end
-    File.write('results.json', JSON.dump(frameworks_hash))
 
     # Begin crawl
-    if scrape_accelerate_only
+    if scrape_test_only == 1
       test_data = sorted_frameworks.select { |data| data["name"] == "Contacts UI" }[0]
       if test_data.empty? == false 
         request_to :parse_framework, url: test_data["href_json"].to_s, data: { name: test_data["name"] }
       end
     else
-
+      sorted_frameworks.each do |current_framework|
+        request_to :parse_framework, url: current_framework["href_json"].to_s, data: { name: current_framework["name"] }
+      end
     end
 
+    rescue StandardError => e
+        puts "There is failed request (#{e.inspect}), skipping it..."
   end
 
   def parse_framework(response, url:, data: {})
     # Get all role:symbol
     # Get all role:collectionGroup to deep search
+    f_name = data[:name]
+    puts "🚀 for the #{f_name} framework..."
 
     # Parse out response json, strip uselss keys
-    framework_json = JSON.parse(response.css("div#json")[0])["references"]
+    framework_json = (JSON.parse(response.css("div#json")[0])["references"] || {})
     stripped_keys = framework_json.map { |k,v| v }
 
     # Append to current json
-    current_json = File.read("results.json")
-    frameworks_hash = JSON.parse(current_json)
-    framework_hash = frameworks_hash[data[:name]]
+    file_name = sanitize_filename f_name
+    current_json = File.read("#{file_name}.json")
+    framework_hash = JSON.parse(current_json)
 
-    # Save any symbols
-    symbols = stripped_keys.select { |symbol| symbol["kind"].downcase == "symbol" }
+    # Save any symbols, the easy part
+    symbols = stripped_keys.select { |symbol| (symbol["kind"] || "").downcase == "symbol" }
     symbols.each do |symbol|
+      next unless (symbol["title"] || "") != ""
       framework_hash["docs"][symbol["title"]] = symbol
     end 
 
     # Save back to json
-    frameworks_hash[data[:name]] = framework_hash
-    File.write('results.json', JSON.dump(frameworks_hash))
+    File.write("#{file_name}.json", JSON.dump(framework_hash))
+
+    # Crawl collectionGroup recursively
+    collection_groups = stripped_keys.select { |cg| (cg["role"] || "").downcase == "collectiongroup" }
+    collection_groups.each do |cg|
+      next unless (cg["url"] || "") != ""
+      request_to :parse_framework, url: 'https://developer.apple.com/tutorials/data' + cg["url"].to_s + '.json', data: { name: f_name }
+    end 
+
+    rescue StandardError => e
+        puts "\n\nThere is failed request to #{f_name} - " + url.to_s + " (#{e.inspect}), skipping it...\n\n"
   end
 
-  def parse_symbol(response, url:, data: {})
-
+  def sanitize_filename(filename)
+    filename.gsub(/[^0-9A-z.\-]/, '_')
   end
   
 end
